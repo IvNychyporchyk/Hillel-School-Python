521f6dc896d9b6042b0df044037e22ef31ffecee
IvNychyporchyk <ivanniciporcuk4@gmail.com>
2022-08-14 22:08:16 +0300

    Homework 5, lesson 5

--- Home_5.py
+++ Home_5.py
@@ -4,18 +4,24 @@
 # Реализовать в 2-х вариантах: используя цикл while и цикл for
 
 
-# Ввести число, проверить на то, что было введено именно число.
-# Возвести его в куб.
-# Реализацию обернуть в вечный цикл с возможностью выйти из него по запросу.
-
 while True:
-    input_value = input("Введите число: ")
-
-    if not input_value.isdigit():
+    input_str = input("Enter an integer n: ")
+    if not input_str.isdigit() or int(input_str) == 0:
+        print("Error, try again!")
         continue
+    input_n = int(input_str)
+    step = 0
+    sum_cubes = 0
+    while input_n > step:
+        step += 1
+        if step % 3 == 0:
+            continue
 
-    print(f'Куб числа равен = {int(input_value)**3}')
+        sum_cubes += int(step) ** 3
+        print("Step: ", step)
+        print("Result cubes: ", sum_cubes)
+    print("The result is as follows", sum_cubes)
 
-    answer = input("Хотите выйти? (y/д): ")
-    if answer in ('y', 'д'):
+    answer = input("Do you want to go out? (Y/D): ")
+    if answer.upper() in ('Y', 'D'):
         break
