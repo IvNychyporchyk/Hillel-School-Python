7715d8822bc6d6b2d627a2757dac7570533f65cc
IvNychyporchyk <58479476+IvNychyporchyk@users.noreply.github.com>
2022-08-16 14:07:35 +0300

    Update Home_6.py
    Guess the number program.

--- Home_6.py
+++ Home_6.py
@@ -1,18 +1,20 @@
 # Сделать программу в виде функций в которой нужно будет угадывать число.
 import random
-guesses_made = 0
-number = random.randint(1, 10)
-print("Так, я загадав число від 1  до 10")
-while guesses_made < 3:
-    guess = int(input("Введіть ваше число: "))
-    guesses_made += 1
-    if guess < number:
-        print("Ваше число менше загаданого.")
-    if guess > number:
-        print("Ваше число більше загаданого.")
-    if guess == number:
-        break
-if guess == number:
-    print("Ух ти! Ви угадали моє число, використавши до {0} спроб!".format(guesses_made))
-else:
-    print("А ось і не вгадав! Я загадав інше число, а саме число {0}".format(number))
+def fun(guesses_made):
+      
+        number = random.randint(1, 10)
+        print("Так, я загадав число від 1  до 10")
+        while guesses_made < 3:
+            guess = int(input("Введіть ваше число: "))
+            guesses_made += 1
+            if guess < number:
+                print("Ваше число менше загаданого.")
+            if guess > number:
+                print("Ваше число більше загаданого.")
+            if guess == number:
+                break
+        if guess == number:
+            print("Ух ти! Ви угадали моє число, використавши до {0} спроб!".format(guesses_made))
+        else:
+            print("А ось і не вгадав! Я загадав інше число, а саме число {0}".format(number))
+fun(0)
